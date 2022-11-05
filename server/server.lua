d3beabdb220042f592eb0942408e6684744d671e
Critzz76 <107814890+Critzz76@users.noreply.github.com>
2022-11-05 02:45:28 -0400

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,4 +1,4 @@
-QBCore = exports['qb-core']:GetCoreObject()
+local QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterServerEvent("Farmcyc:recoleccion", function()
 	local src = source
@@ -18,6 +18,7 @@ QBCore.Functions.CreateCallback('Farmcyc:empaquetado:cangrejos', function (sourc
 	local Player  = QBCore.Functions.GetPlayer(src)
 	if Player.Functions.GetItemByName('crabs') then
 		Player.Functions.RemoveItem('crabs', 5)
+			TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['crabs'], "remove")
 			Wait(1000)
 		Player.Functions.AddItem('crayfish', 1)
 	else
@@ -31,6 +32,7 @@ QBCore.Functions.CreateCallback('Farmcyc:empaquetado:camarones', function (sourc
 	local Player  = QBCore.Functions.GetPlayer(src)
 	if Player.Functions.GetItemByName('shrimp') then
 		Player.Functions.RemoveItem('shrimp', 4)
+			TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['shrimp'], "remove")
 			Wait(1000)
 		Player.Functions.AddItem('shrimpbox', 1)
 	else
@@ -46,6 +48,7 @@ RegisterNetEvent('Ventadecamarones', function()
     local PrecioCamarones = Config.PCamaron
 	if Player.Functions.GetItemByName('shrimpbox') then
 		Player.Functions.RemoveItem('shrimpbox', 1)
+			TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['shrimpbox'], "remove")
 		Player.Functions.AddMoney('cash', PrecioCamarones)
 	else
 		TriggerClientEvent('QBCore:Notify', src, "You have no Shrimps", 'error')
@@ -60,6 +63,7 @@ RegisterNetEvent('Ventadecangrejos', function()
     local PrecioCangrejos = Config.PCangrejo
 	if Player.Functions.GetItemByName('crayfish') then
 		Player.Functions.RemoveItem('crayfish', 1)
+			TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['crayfish'], "remove")
 		Player.Functions.AddMoney('cash', PrecioCangrejos)
 	else
 		TriggerClientEvent('QBCore:Notify', src, "You have no crabs", 'error')
